b4f53e6c4e073af58b1de4c19f68cf9d4d7598aa
nicolas.souleman@gmail.com <nicolas.souleman@gmail.com@8365c4ea-d8fd-565e-e71d-ff2f829bd917>
2012-03-05 17:12:37 +0000

    ajout script perl qui injecte les donnees dans pg
    git-svn-id: https://house-on-wire.googlecode.com/svn/trunk@6 8365c4ea-d8fd-565e-e71d-ff2f829bd917

---
+++ scripts/perl/teleinfo_pgsql.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+
+# Home On Wire
+
+use Device::SerialPort;
+use Config::IniHash;
+use DateTime;
+use DBI;
+use vars qw/ %options /;
+
+sub init ()
+{
+        use Getopt::Std;
+        getopts("f:",\%options) or usage();
+        usage() if not $options{f};
+}
+sub usage ()
+{
+	if ( ! -f '/etc/house-on-wire/house-on-wire.ini')
+	{
+        	print ("Usage : $0 -f <path_to_config_file>\nDefault config file /etc/house-on-wire/house-on-wire.ini not found");
+		exit (1);
+	}
+}	
+
+&init;
+
+if (exists $options{f}) 
+{
+	$config_file=$options{f};
+}
+else 
+{
+	$config_file='/etc/house-on-wire/house-on-wire.ini';
+}
+
+my $cfg = ReadINI "$config_file";
+my $database=$cfg->{bdd}->{dbname};
+my $hostname=$cfg->{bdd}->{host};
+my $login=$cfg->{bdd}->{username};
+my $password=$cfg->{bdd}->{password};
+my $dbport=$cfg->{bdd}->{port};
+
+my $port = Device::SerialPort->new("/dev/ttyUSB0");
+$port->databits($cfg->{teleinfo_serial}->{databits});
+$port->baudrate($cfg->{teleinfo_serial}->{baudrate});
+$port->parity($cfg->{teleinfo_serial}->{parity});
+$port->stopbits($cfg->{teleinfo_serial}->{stopbits});
+$port->read_char_time (1); #Evite le CPU load 100
+
+my $line='';
+my $spacecount=0;
+
+while(1) {
+	my $byte=$port->read(1);
+	$spacecount++ if ($byte eq chr(32)); #Detection caractere  SPACE
+	if (($byte) and ($byte eq chr(2))) #Detection caractere Caractere STX
+	{
+		$line='';
+		$maintenant = DateTime->now();
+		$datetime = ($maintenant->date()." ".$maintenant->time());
+		$line=($maintenant->epoch().";".$maintenant->date().";".$maintenant->time().";");
+		chomp $line;
+	}
+	if (($byte) and ($byte eq chr(3))) # Detection caractere Caractere ETX
+	{
+		if ($line =~ /^(\d{10});(\d{4}-\d{2}-\d{2});(\d{2}:\d{2}:\d{2});.ADCO (\d{12});OPTARIF (....);ISOUSC (\d{2});HCHC (\d{9});HCHP (\d{9});PTEC (....);IINST (\d{3});IMAX (\d{3});PAPP (\d{5});HHPHC (.);MOTDETAT (\d{6});$/)
+		{
+			print "Format ligne OK : $line\n";
+			my $dbi=DBI->connect("DBI:Pg:dbname=$database;host=$hostname;port=$dbport","$login","$password") or die "Erreur pendant l'ouverture de la base de Donnée MySQL $DBI::errstr";
+            		$dbi->do("insert into teleinfo (timestamp,rec_date,rec_time,adco,optarif,isousc,hchc,hchp,ptec,inst1,imax1,papp,hhphc,motdetat, date) values ($1,'$2','$3','$4','$5',$6,$7,$8,'$9'::varchar(2),$10,$11,$12,'$13','$14', '$datetime')");
+			$dbi->disconnect;
+		}
+		else
+		{
+			print "Format ligne NOK : $line\n";
+		}
+	} 
+	if ($byte eq chr(13)) #Detection caractere Carriage Return
+	{
+		$line="$line".';';
+		$spacecount=0;
+	}
+	elsif (($byte ne chr(10)) and ($spacecount < 2)) #Detection Caractere LineFeed
+  	{	
+		$line="$line".$byte;
+	}
+}
+
