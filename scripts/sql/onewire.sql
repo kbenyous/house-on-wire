512b34f099c92d5ae35c37af75da1581a2ed1ac0
pyvitre@gmail.com <pyvitre@gmail.com@8365c4ea-d8fd-565e-e71d-ff2f829bd917>
2012-03-13 22:14:41 +0000

    Create table onewire
    git-svn-id: https://house-on-wire.googlecode.com/svn/trunk@41 8365c4ea-d8fd-565e-e71d-ff2f829bd917

--- scripts/sql/onewire.sql
+++ scripts/sql/onewire.sql
@@ -4,3 +4,9 @@ CREATE TABLE onewire_data (
 	value varchar(32) NOT NULL
 );
 
+CREATE TABLE onewire (
+  id varchar(32) NOT NULL,
+	name text NOT NULL,
+  unity varchar(32),
+  last_update timestamp without time zone
+);
