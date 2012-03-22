b35d1a6401fa56766ca7a088e70b0590ff6f08ff
mickael.le.baillif@gmail.com <mickael.le.baillif@gmail.com@8365c4ea-d8fd-565e-e71d-ff2f829bd917>
2012-03-22 14:04:26 +0000

    - l'aggregat se fait uniquement sur 5min, à partir de J-3 - la fonction d'historisation calcule tous les aggrégats manquants jusqu'à J-3 - le calcul du cout journalier est transféré dans une fonction, qui prend en argument le jour que l'on veut calculer - création d'une vue teleinfo_view qui regroupe les aggregats jusqu'à J-3 et les données complètes sur les dernières 48h
    git-svn-id: https://house-on-wire.googlecode.com/svn/trunk@45 8365c4ea-d8fd-565e-e71d-ff2f829bd917

--- scripts/sql/teleinfo.sql
+++ scripts/sql/teleinfo.sql
@@ -17,6 +17,9 @@ CREATE TABLE teleinfo (
 
 ALTER TABLE ONLY teleinfo
     ADD CONSTRAINT teleinfo_pkey PRIMARY KEY (date);
+    
+-- Pour accélerer le calcul des MIN et MAX sur PAPP
+CREATE INDEX idx_papp ON teleinfo (papp);
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
@@ -31,7 +34,7 @@ CREATE TABLE teleinfo_cout (
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-CREATE TABLE teleinfo_histo (
+CREATE TABLE teleinfo_agg (
     date timestamp with time zone NOT NULL,
     ptec character varying(2) NOT NULL,
     papp numeric,
@@ -46,18 +49,12 @@ CREATE TABLE teleinfo_histo (
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-CREATE TABLE teleinfo_minute (
-    date timestamp without time zone NOT NULL,
-    ptec character varying(2) NOT NULL,
-    papp numeric,
-    papp_min integer,
-    papp_max integer,
-    iinst numeric,
-    iinst_min integer,
-    iinst_max integer,
-    hchc integer,
-    hchp integer
-);
+CREATE VIEW teleinfo_view AS
+    SELECT date, ptec, ROUND(papp) AS papp, hchc, hchp FROM teleinfo_agg
+    UNION ALL
+    SELECT date, ptec, papp, hchc, hchp FROM teleinfo
+        WHERE date >= ( CURRENT_DATE - INTERVAL '1 day' )
+ORDER BY date ASC;
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
@@ -66,14 +63,47 @@ ALTER TABLE ONLY teleinfo_cout
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-ALTER TABLE ONLY teleinfo_histo
-    ADD CONSTRAINT teleinfo_histo_pkey PRIMARY KEY (date, ptec);
+ALTER TABLE ONLY teleinfo_agg
+    ADD CONSTRAINT teleinfo_agg_pkey PRIMARY KEY (date, ptec);
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-ALTER TABLE ONLY teleinfo_minute
-    ADD CONSTRAINT teleinfo_minute_pkey PRIMARY KEY (date, ptec);
+-- Calcul du cout de l'électricité pour le jour spécifié en arg
+--  arg=1 ==> la veille
+--  arg=2 ==> l'avant-veille, etc
 
+CREATE FUNCTION teleinfo_calcul_cout( IN int ) 
+RETURNS void AS $$
+INSERT INTO teleinfo_cout
+SELECT CURRENT_DATE - CAST( (CAST($1 AS TEXT) || ' day') AS interval) AS jour,
+            hchc_max - hchc_min AS hchc,
+            hchp_max - hchp_min AS hchp,
+            0.0895 * ((hchc_max - hchc_min) / 1000) AS cout_hc,
+            0.1312 * ((hchp_max - hchp_min) / 1000) AS cout_hp,
+            94.06 / 365 AS cout_abo
+    FROM (
+        SELECT
+            MIN(hchc) as hchc_min,
+            MAX(hchc) as hchc_max,
+            MIN(hchp) as hchp_min,
+            MAX(hchp) as hchp_max
+        FROM (
+            (SELECT
+                date,
+                hchc,
+                hchp
+                FROM teleinfo WHERE date BETWEEN (CURRENT_DATE - CAST( (CAST($1 AS TEXT) || ' day') AS interval)) AND (CURRENT_DATE - CAST( (CAST( ($1-1) AS TEXT) || ' day') AS interval)) ORDER BY date ASC LIMIT 1
+            )
+            UNION ALL
+            (SELECT
+                date,
+                hchc,
+                hchp
+                FROM teleinfo WHERE date BETWEEN (CURRENT_DATE - CAST( (CAST($1 AS TEXT) || ' day') AS interval)) AND (CURRENT_DATE - CAST( (CAST( ($1-1) AS TEXT) || ' day') AS interval)) ORDER BY date DESC LIMIT 1
+            )
+            ) foo
+    ) foo2;
+$$ language sql;
 
 -- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
@@ -82,36 +112,14 @@ ALTER TABLE ONLY teleinfo_minute
 -- Batch a lancer dans la nuit
 --
 -- Objectif :
--- 48 dernieres heures en full ( env 1 enreg / sec )
--- J-3 a J-7 en 1 point par 1 min
--- J-8 a ... en 1 point par 5 min
-
--- Creation de données agrégée sur 1 min
-CREATE FUNCTION historisation_teleinfo() RETURNS void AS $$
-INSERT INTO teleinfo_minute
-SELECT 
-    date_trunc('minute', date) as date,
-    ptec,
-    avg(papp) as papp, 
-    min(papp) as papp_min,
-    max(papp) as papp_max,
-    avg(iinst) as inst1, 
-    min(iinst) as inst1_min,
-    max(iinst) as inst1_max,
-    max(hchc) as hchc,
-    max(hchp) as hchp
-
-FROM    
-    teleinfo 
-WHERE 
-    date_trunc('day', date) = current_date - interval '1 day' 
-GROUP BY
-    date_trunc('minute', date), ptec;
-
 -- Creation des données agregée sur 5 min
-INSERT INTO teleinfo_histo
+-- 48 dernieres heures en full ( env 1 enreg / sec )
+--  à partir de J-3, 1 point par 5 min
+
+CREATE FUNCTION historisation_teleinfo() RETURNS void AS $$
+INSERT INTO teleinfo_agg
 SELECT
-    to_timestamp( trunc(EXTRACT(EPOCH FROM date)/300)*300 ), 
+    TO_TIMESTAMP( TRUNC(EXTRACT(EPOCH FROM date)/300)*300 ) AS temps, 
     ptec, 
     avg(papp) as papp, 
     min(papp) as papp_min,
@@ -121,37 +129,25 @@ SELECT
     max(iinst) as iinst_max,
     max(hchc) as hchc,
     max(hchp) as hchp
-
 FROM    
     teleinfo 
 WHERE 
-    date_trunc('day', date) = current_date - interval '1 day' 
+    date BETWEEN
+            COALESCE(   DATE_TRUNC('day', (SELECT MAX(date) FROM teleinfo_agg)),  -- dernier jour aggregé
+                        '1970-01-01' )          -- à défaut, l'origine des temps
+            + interval '1 day'                  -- on commence au lendemain
+         AND (CURRENT_DATE - interval '1 day')  -- on n'aggrège pas les 48 dernières heures
 GROUP BY
-    to_timestamp( trunc(EXTRACT(EPOCH FROM date)/300)*300 ), ptec;
+    temps, ptec;
 
-
-
--- MISE A JOUR DE LA TABLE DES COUTS
-INSERT INTO teleinfo_cout 
-SELECT
-current_date - interval '1 day',
-hchc_max - hchc_min AS hchc,
-hchp_max - hchp_min AS hchp,
-0.0895 * ((hchc_max - hchc_min) / 1000) AS cout_hc,
-0.1312 * ((hchp_max - hchp_min) / 1000) AS cout_hp,
-94.06 / 365 as cout_abo
-FROM
-(
-SELECT
-    (SELECT min(hchc) FROM teleinfo WHERE date_trunc('day', date) = current_date )::integer AS hchc_max,
-    (SELECT min(hchp) FROM teleinfo WHERE date_trunc('day', date) = current_date )::integer AS hchp_max,
-    (SELECT min(hchc) FROM teleinfo WHERE date_trunc('day', date) = current_date - interval '1 day')::integer AS hchc_min,
-    (SELECT min(hchp) FROM teleinfo WHERE date_trunc('day', date) = current_date - interval '1 day')::integer AS hchp_min
-)foo;
 
 -- Suppression des données dont on ne veut plus
 --     DELETE FROM teleinfo WHERE date < current_date - interval '5 days';
---     DELETE FROM teleinfo_minute WHERE date < current_date - interval '15 days';
+
+
+-- Calcul du cout de la veille
+SELECT teleinfo_calcul_cout( 1 );
+
 
 $$ LANGUAGE sql;
 
