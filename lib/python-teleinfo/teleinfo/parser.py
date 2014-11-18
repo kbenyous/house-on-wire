fb1b617b76a7062bf6a01b5df049e8c5dc2b3044
Mickael Le Baillif <mlebaillif@hi-media.com>
2014-11-18 12:51:56 +0100

    synchro trame en debut d'acquisition, + divers fix

--- lib/python-teleinfo/teleinfo/parser.py
+++ lib/python-teleinfo/teleinfo/parser.py
@@ -22,23 +22,26 @@ class Parser:
 
     def get_frame(self):
         raw = self._get_raw_frame().strip(self.MARKER_END_LINE)
-        groups = [line.split(" ", 2) for line in raw.split(self.MARKER_END_LINE)]
-        frame = dict([
-            (k, v) for k, v, chksum in groups if chksum == self._checksum(k, v)
-        ])
-        if len(frame) != len(groups):
-            logger.info("Discarded fields because of bad checksum: {}".format(
-                itertools.ifilterfalse(lambda g: g[2] != self._checksum(g[0], g[1]), groups)
-            ))
+        try:
+            groups = [line.split(" ", 2) for line in raw.split(self.MARKER_END_LINE)]
+            frame = dict([
+                (k, v) for k, v, chksum in groups if chksum == self._checksum(k, v)
+            ])
+            if len(frame) != len(groups):
+                logger.info("Discarded fields because of bad checksum: {}".format(
+                    [f for f in itertools.ifilterfalse(lambda g: g[2] == self._checksum(g[0], g[1]), groups)]
+                ))
+        except Exception as e:
+            logger.error("Caught exception while parsing teleinfo frame: {}".format(e))
+            frame = {}
         return frame
 
     def _synchro_debut_trame(self):
-        c = 0
         while self._hw.read_char() != self.MARKER_START_FRAME:
-            c += 1
-        logger.debug("Skipped {} bytes while synchronizing for start of frame".format(c))
+            pass
 
     def _get_raw_frame(self):
+        self._synchro_debut_trame()
         frame = ''.join(itertools.takewhile(
             lambda c: c != self.MARKER_STOP_FRAME,
             self._hw)
